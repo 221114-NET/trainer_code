8d9657286c42e00569302d1da548b9bba1110c5d
Mark Moore <mark.moore@revature.com>
2022-11-22 14:34:07 -0600

    small change to MarksDrawrings

--- demos/StackDemo/Program.cs
+++ demos/StackDemo/Program.cs
@@ -10,9 +10,9 @@ class Program
         StackO stack1 = new StackO();
         Person p = new Person("Martin", "Brody", "We're gonna need a bigger boat...");
         stack1.Push(p);
-        Person p1 = new Person("Anakin", "Skywalker", "Luke, I am your father");
+        Person p1 = new Person("Anakin", "Skywalker", "Luke, I am your father.");
         stack1.Push(p1);
-        Person p2 = new Person("T", "100", "I'll be bahhhk");
+        Person p2 = new Person("T", "100", "I'll be bahhhk.");
         stack1.Push(p2);
 
         stack1.StackTrace();
@@ -29,8 +29,5 @@ class Program
 
         Person p5 = stack1.Peek();
         Console.WriteLine($"The peeked person is {p5.Fname} {p5.Lname}");
-
-
-
     }
 }
