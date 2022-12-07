dda776072044164994fb2f6771ffab8f2819f1a2
Mark Moore <mark.moore@revature.com>
2022-12-07 11:35:07 -0600

    adds a array filtering method

---
+++ demos/FindMin/Program.cs
@@ -0,0 +1,36 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        int[] originalArr = { 1, 2, 3, 400, -5, 6, 7, -88, 9, 34 };
+
+        int res = FindMin(originalArr);
+        Console.WriteLine(res);
+    }
+
+    /// <summary>
+    /// find the smallest int in the array
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public static int FindMin(int[] arr)
+    {
+
+        // solution 1#
+        // int min = arr[0];
+        // foreach (int x in arr)
+        // {
+        //     if (x > min)
+        //     {
+        //         min = x;
+        //     }
+        // }
+
+        // solution #2
+        int min = arr.Max();
+        return min;
+    }
+}
